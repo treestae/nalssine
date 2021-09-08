da9dbf9fea84f9527ff9f08dae2e1b83098906a5
Tae <treestae@gmail.com>
2021-09-08 22:54:10 +0900

    첫 홈페이지 완성 * SNS 버튼 연결 필요 * home을 index.html로 변경 필요 * 냥큐브 시뮬레이터의 전체화면에서 뒤로가기 버튼

--- home.js
+++ home.js
@@ -6,9 +6,10 @@ const port = 8080
 //app.use(compression());
 app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
-  res.send(`안녕하세요. 날씨네 입니다.
+  res.sendFile(__dirname + '/index.html');
+  /*res.send(`안녕하세요. 날씨네 입니다.
   지금은 홈페이지를 준비하는 중이니 잠시만 기다려주세요.
-  <a href="/ncube/calc>냥큐브 판 수 계산기</a>"`);
+  <a href="/ncube/calc>냥큐브 판 수 계산기</a>"`);*/
 });
 
 app.get('/ncube/calc', (req, res) => {
