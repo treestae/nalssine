798b22ce29751a1cf10fb4a7fb9bd349c484bcd1
treestae <treestae@gmail.com>
2021-11-11 11:01:25 +0900

    version 1.0.1 - /naver URL로 마지막 인증번호 확인 가능 디폴트 000000 - /naver/숫자 로 인증번호 입력 가능

--- home.js
+++ home.js
@@ -1,9 +1,11 @@
 const express = require('express')
-//const compression = require('compression');
+
 const app = express()
 const port = 8080
 
-//app.use(compression());
+// 네이버 스마트스토어 인증번호
+let smartstore = "000000"
+
 app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -14,6 +16,15 @@ app.get('/', (req, res) => {
 
 app.get('/ncube/calc', (req, res) => {
   res.sendFile(__dirname + '/ncube/calc/index.html');
+});
+
+app.get('/naver', (req, res) => {
+  res.send(smartstore);
+});
+
+app.get('/naver/:num', (req, res) => {
+  smartstore = req.params.num
+  res.status(200).end()
 });
 
 app.listen(port, () => {
