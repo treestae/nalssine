7eff957bf342b21022967dbe72492801a30dd60c
treestae <treestae@gmail.com>
2021-11-19 14:38:32 +0900

    * 주석 제거 + 홈페이지 버튼 링크 연결 - 사이드 메뉴 중 자작나무, 멀바우 버튼 제거

--- home.js
+++ home.js
@@ -7,11 +7,9 @@ const port = 8080
 let smartstore = "000000"
 
 app.use(express.static(__dirname + '/'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-  /*res.send(`안녕하세요. 날씨네 입니다.
-  지금은 홈페이지를 준비하는 중이니 잠시만 기다려주세요.
-  <a href="/ncube/calc>냥큐브 판 수 계산기</a>"`);*/
+  res.sendFile(__dirname + '/index.html');  
 });
 
 app.get('/ncube/calc', (req, res) => {
