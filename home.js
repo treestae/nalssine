2efb192771b5bc6c14887bf82406cd66b6a39168
treestae <treestae@gmail.com>
2022-04-15 09:06:09 +0900

    * 미사용 버튼 회색으로 변경 * 주석 추가

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-// 네이버 스마트스토어 인증번호
+// 네이버 스마트스토어 인증번호 저장용
 let smartstore = "000000"
 
 app.use(express.static(__dirname + '/'));
@@ -12,14 +12,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');  
 });
 
+// 판수 계산기
 app.get('/ncube/calc', (req, res) => {
   res.sendFile(__dirname + '/ncube/calc/index.html');
 });
 
+// 스마트스토어 인증번호 출력
 app.get('/naver', (req, res) => {
   res.send(smartstore);
 });
 
+// 스마트스토어 인증번호 입력
 app.get('/naver/:num', (req, res) => {
   smartstore = req.params.num
   res.status(200).end()
